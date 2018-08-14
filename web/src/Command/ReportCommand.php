51fc1f636cd7d6db134a1653ae6bd9503ff2791c
alexandervishnivetskiy <vishnivetskiy.alexandr@gmail.com>
2018-08-14 11:30:18 +0300

    add report from command line v.1

--- web/src/Command/ReportCommand.php
+++ web/src/Command/ReportCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
-class CreateUserCommand extends Command\Command
+class ReportCommand extends Command\Command
 {
 
     protected $em;
