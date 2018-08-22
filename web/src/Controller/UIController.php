5f6993fbcfa2d3877efb3aec4aeebc8337c3684b
alexandervishnivetskiy <vishnivetskiy.alexandr@gmail.com>
2018-08-22 17:19:49 +0300

    work with twig template

---
+++ web/src/Controller/UIController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+class UIController extends Controller
+{
+    /**
+     * @Route("/", name="home_page")
+     */
+    public function renderClientTable(Request $request)
+    {
+        $clients = $this->getDoctrine()->getRepository(Client::class)->findAll();
+        $clientName = array();
+        foreach ($clients as $client) {
+            $clientName[$client->getName()] = $client->getID();
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('select', ChoiceType::class,
+                array('choices' => $clientName, 'attr' => array('class' => 'form-control')))
+            ->add('submit', SubmitType::class,
+                array('attr' => array('class' => 'form-control mt-3 bg-success')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $id = $form->getData();
+            $clients = $this->getDoctrine()->getRepository(Client::class)->findBy(array('id' => $id));
+            $reportsArray = array();
+            $reports = $clients[0]->getReports();
+            foreach ($reports as $report) {
+                $arr = array();
+                $arr['id'] = $report->getID();
+                $arr['name'] = $report->getName();
+                $arr['deviceID'] = $report->getDeviceID();
+                $arr['description'] = $report->getDescription();
+                $arr['client'] = $report->getClient()->getName();
+                $reportsArray[] = $arr;
+            }
+
+            return $this->render('clients/reports.html.twig', array('reports' => $reportsArray));
+        }
+        return $this->render('clients/index.html.twig', array('form' => $form->createView()));
+    }
+}
--- web/src/DataFixtures/ClientFixtures.php
+++ web/src/DataFixtures/ClientFixtures.php
@@ -16,24 +16,28 @@ class ClientFixtures extends Fixture
 
     {
         $faker = Factory::create();
-        $count = rand(1,10);
-        $client = new Client();
-        for ($i = 0; $i < $count; $i++) {
-            $client->setName($faker->name);
-            $client->setCountry($faker->country);
-            $client->setTelephone($faker->phoneNumber);
-            $client->setCountry($faker->country);
-            $client->setEmail($faker->email);
-            $manager->persist($client);
+        $count = rand(1, 10);
 
-            $report = new Report();
-            $report->setName($faker->text(20));
-            $report->setDeviceID($faker->numberBetween(1, 10000));
-            $report->setDescription($faker->text);
-            $report->setClient($client);
-            $manager->persist($report);
+        for ($j = 0; $j < $count; $j++) {
+            $client = new Client();
+            for ($i = 0; $i < $count; $i++) {
 
-            $manager->flush();
+                $client->setName($faker->name);
+                $client->setCountry($faker->country);
+                $client->setTelephone($faker->phoneNumber);
+                $client->setCountry($faker->country);
+                $client->setEmail($faker->email);
+                $manager->persist($client);
+
+                $report = new Report();
+                $report->setName($faker->text(20));
+                $report->setDeviceID($faker->numberBetween(1, 10000));
+                $report->setDescription($faker->text);
+                $report->setClient($client);
+                $manager->persist($report);
+
+                $manager->flush();
+            }
         }
 
     }
